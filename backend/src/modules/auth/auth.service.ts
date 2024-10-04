ea28e86db2f350f75b669cfa011116415f5297e1
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-04 06:05:08 +0300

    refactor: Remove unused code and dependencies in auth service

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,12 +1,7 @@
-interface UserWithToken extends Omit<User, "password"> {
-  token?: string; // or string | null if you want to allow null
-}
-
 import prisma from "../../prisma/prisma";
-import { User } from "@prisma/client";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import { ClientSideError } from "../../utils/errors/clientSideError";
+import generateAuthToken from "../../utils/createToken";
 
 export const loginService = async (email: string, inputPassword: string) => {
   const userWithPassword = await prisma.user.findUnique({
@@ -32,15 +27,4 @@ const isPasswordValid = async (
 ) => {
   if (!password) return false;
   return bcrypt.compare(inputPassword, password);
-};
-
-const generateAuthToken = (user: UserWithToken) => {
-  if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined");
-  }
-  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-
-  user.token = token;
 };
