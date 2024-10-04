2dbd223d334b7dd8024f9767c6860a3a5f4353d6
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-04 14:12:32 +0300

    refactor: Update validateMiddleware to support optional params validation

--- backend/src/modules/middlewares/validateMiddleware.ts
+++ backend/src/modules/middlewares/validateMiddleware.ts
@@ -1,13 +1,22 @@
 import { z, ZodError } from "zod";
-
 import { Request, Response, NextFunction } from "express";
 import { ServerSideError } from "../../utils/errors/serverSideError";
 import { ClientSideError } from "../../utils/errors/clientSideError";
 
-export const validate = (schema: z.ZodSchema<any>) => {
+export const validate = (
+  schema: z.ZodSchema<any> = z.any(), // Default schema for body
+  paramsSchema?: z.ZodSchema<any> // Optional schema for params
+) => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
+      // Validate body (only if schema is provided and it's not z.any())
       schema.parse(req.body);
+
+      // Validate params if paramsSchema is provided
+      if (paramsSchema) {
+        paramsSchema.parse(req.params);
+      }
+
       next();
     } catch (error) {
       if (error instanceof ZodError) {
