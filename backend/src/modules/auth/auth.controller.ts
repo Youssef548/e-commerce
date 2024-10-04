d1744fbff6657e6f62c3f0b64d1a6dca36980c2d
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-04 07:04:24 +0300

    feat: add user login validation

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -2,13 +2,9 @@
 import { Request, Response } from "express";
 import { loginService } from "./auth.service";
 import asyncHandler from "../middlewares/asyncHandler";
-import { ClientSideError } from "../../utils/errors/clientSideError";
 
 export const login = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body;
-
-  if (!email || !password)
-    throw new ClientSideError("Email and password are required", 400);
 
   const user = await loginService(email, password);
 
--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -1,9 +1,11 @@
 // src/modules/auth/auth.routes.ts
 import { Router } from "express";
 import { login } from "./auth.controller";
+import { validate } from "../middlewares/validateMiddleware";
+import { loginSchema } from "./schemas";
 
 const router = Router();
 
-router.post("/login", login);
+router.post("/login", validate(loginSchema), login);
 
 export default router;
