e1c8d8df2a7886ed62c26521af87fb1d89050135
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-06 01:53:31 +0300

    feat: Add Navigation component to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Navigation from "./pages/Auth/Navigation";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <ToastContainer />
+      <Navigation />
       <main>
         <Outlet />
       </main>
