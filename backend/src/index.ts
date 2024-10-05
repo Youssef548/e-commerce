ad9d7c07cfc3c7041b125d751f4a79b535d78773
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-06 01:58:36 +0300

    feat: Add CORS middleware to enable cross-origin resource sharing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,13 @@ import userRoutes from "./modules/users/user.routes";
 import authRoutes from "./modules/auth/auth.routes";
 import errorHandler from "./modules/middlewares/globalErrorHandler";
 import swaggerSetup from "./swagger";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 
 // Mount user routes
+app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
