310166ea9421518905f73fe9a0c43beec8dc92fe
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-06 01:53:22 +0300

    feat: Add ToastContainer and Outlet components to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ToastContainer />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
