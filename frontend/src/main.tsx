6e548c451352aa6d23b2965abf8b5e7705ce6ba9
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-06 01:52:32 +0300

    feat: SetupRoutes and login routes on main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
+import Login from "./pages/Auth/Login.tsx";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+    </Route>
+  )
+);
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
