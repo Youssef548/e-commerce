af0f7526db79768c0dcb5be651d6663b8e75d434
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-06 01:47:55 +0300

    feat: add users , auth slices and constants too

---
+++ frontend/src/redux/api/usersApiSlice.ts
@@ -0,0 +1,16 @@
+import { apiSlice } from "./apiSlice";
+import { AUTH_URL } from "../constants";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${AUTH_URL}/login`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
--- frontend/src/redux/constants.ts
+++ frontend/src/redux/constants.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = "http://localhost:5000";
+export const AUTH_URL = "/api/auth";
+export const USERS_URL = "/api/users";
