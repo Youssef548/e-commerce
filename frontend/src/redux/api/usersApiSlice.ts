e9ee3ea90dc1ba69bb29e6d093ab7b50f342b13e
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-09 00:00:28 +0300

    refactor: Add deleteUser mutation to usersApiSlice

--- frontend/src/redux/api/usersApiSlice.ts
+++ frontend/src/redux/api/usersApiSlice.ts
@@ -37,6 +37,13 @@ export const usersApiSlice = apiSlice.injectEndpoints({
       providesTags: ["User"],
       keepUnusedDataFor: 5,
     }),
+
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: "DELETE",
+      }),
+    }),
   }),
 });
 
