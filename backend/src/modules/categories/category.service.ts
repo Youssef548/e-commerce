0c4b8ffdce522af2d9279d1b965155d4059a88a0
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-11 04:58:54 +0300

    fix: Throw error if category already exists in createCategoryService

--- backend/src/modules/categories/category.service.ts
+++ backend/src/modules/categories/category.service.ts
@@ -3,8 +3,8 @@ import { ClientSideError } from "../../utils/errors/clientSideError";
 export const createCategoryService = async (name: string) => {
   const categoryExists = await prisma.category.findUnique({ where: { name } });
 
-  if (!categoryExists) {
-    throw new ClientSideError("Category not found !", 404);
+  if (categoryExists) {
+    throw new ClientSideError("Category already exits", 404);
   }
 
   const category = await prisma.category.create({ data: { name } });
