c43204c10771ecef40b5f71aabca7bf5e92b2fa5
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-14 00:24:59 +0300

    feat(service): add updateCategoryService to handle category updates

--- backend/src/modules/categories/category.service.ts
+++ backend/src/modules/categories/category.service.ts
@@ -10,3 +10,25 @@ export const createCategoryService = async (name: string) => {
   const category = await prisma.category.create({ data: { name } });
   return category;
 };
+
+export const updateCategoryService = async (
+  id: string,
+  data: { name: string }
+) => {
+  const { name } = data;
+  const categoryId = parseInt(id);
+  const categoryExists = await prisma.category.findUnique({
+    where: { id: categoryId },
+  });
+
+  if (!categoryExists) {
+    throw new ClientSideError("Category not found", 404);
+  }
+
+  const updatedCategory = await prisma.category.update({
+    where: { id: categoryId },
+    data: { name },
+  });
+
+  return updatedCategory;
+};
