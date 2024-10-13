4f402f43a1892a72090abcf15c2756afbe34ba4e
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-14 00:25:08 +0300

    feat(controller): add updateCategoryController for updating categories

--- backend/src/modules/categories/category.controller.ts
+++ backend/src/modules/categories/category.controller.ts
@@ -1,15 +1,23 @@
-import { Request, Response } from "express";
+import { Request } from "express";
 import asyncHandler from "../middlewares/asyncHandler";
-import { createCategoryService } from "./category.service";
+import {
+  createCategoryService,
+  updateCategoryService,
+} from "./category.service";
 
-export const createCategory = asyncHandler(
-  async (req: Request, res: Response) => {
-    const { name } = req.body;
+export const createCategory = asyncHandler(async (req: Request) => {
+  const { name } = req.body;
 
-    const category = await createCategoryService(name);
-    return category;
-  },
-  201
-);
+  const category = await createCategoryService(name);
+  return category;
+}, 201);
 
+export const updateCategory = asyncHandler(async (req: Request) => {
+  const { categoryId } = req.params;
+  const { name } = req.body;
+  const data = { name } as { name: string };
 
+  const category = await updateCategoryService(categoryId, data);
+
+  return category;
+}, 200);
