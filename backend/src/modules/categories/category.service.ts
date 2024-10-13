af12b60911fc490e885146db9badeb13af9993e5
Youssef548 <youssefahmedpvp@gmail.com>
2024-10-13 22:57:40 +0300

    fix: Update error status code for existing category check in createCategoryService

--- backend/src/modules/categories/category.service.ts
+++ backend/src/modules/categories/category.service.ts
@@ -4,7 +4,7 @@ export const createCategoryService = async (name: string) => {
   const categoryExists = await prisma.category.findUnique({ where: { name } });
 
   if (categoryExists) {
-    throw new ClientSideError("Category already exits", 404);
+    throw new ClientSideError("Category already exits", 400);
   }
 
   const category = await prisma.category.create({ data: { name } });
